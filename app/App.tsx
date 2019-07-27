98f6176c459208718f89f071dace21708df1aac5
Black-Tomcat <tomcat.frantz@gmail.com>
2019-07-27 14:35:16 +1000

    More dependencies and basic starting point

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RootNavigator from "./screens/navigators/RootNavigator";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component<{}, {}> {
+  render() {
+    return <RootNavigator/>;
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ app/screens/navigators/RootNavigator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+interface RootNavigatorProps {
+}
+
+interface RootNavigatorState {
+}
+
+export default class RootNavigator extends React.Component<RootNavigatorProps, RootNavigatorState> {
+    render() {
+        return (null)
+    }
+}
