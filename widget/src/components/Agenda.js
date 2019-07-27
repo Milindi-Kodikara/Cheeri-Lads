549b8bbfebba5c35bf8e4b56143922917cf6da92
Sam Nolan <samnolan555@gmail.com>
2019-07-28 04:04:52 +1000

    Localize widget to calendar

--- widget/src/components/Agenda.js
+++ widget/src/components/Agenda.js
@@ -6,47 +6,76 @@ export default class Agenda extends React.Component {
   render() {
     return (
       <div>
-        <h1>Agenda</h1>
         <Query
           query={gql`
-            {
-              allEvents {
-                edges {
-                  node {
-                    id
-                    name
-                    location
-                    feed {
-                      name
-                      color
+            query getCalendar($calendarId: ID!) {
+              calendar(id: $calendarId) {
+                name
+                feeds {
+                  edges {
+                    node {
+                      events {
+                        edges {
+                          node {
+                            id
+                            name
+                            location
+                            feed {
+                              name
+                              color
+                            }
+                            start
+                            end
+                            description
+                          }
+                        }
+                      }
                     }
-                    start
-                    end
-                    description
                   }
                 }
               }
             }
-          `}>
+          `}
+          variables={{
+            calendarId: new URLSearchParams(window.location.search).get(
+              'calendar',
+            ),
+          }}>
           {({loading, error, data}) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error Occurred</p>;
+            let name = data.calendar.name;
+            let events = data.calendar.feeds.edges.reduce(
+              (total, current) =>
+                total.concat(current.node.events.edges.map(edge => edge.node)),
+              [],
+            );
 
-            return data.allEvents.edges
-              .map(node => node.node)
-              .map(({id, name, feed, description, start, end, location}) => (
-                <div key={id}>
-                  <h2>{name}</h2>
-                  <h3>Brought to you by {feed.name}</h3>
-                  <ul>
-                    <li>
-                      From {start} to {end}
-                    </li>
-                    <li>At {location}</li>
-                  </ul>
-                  <p>{description}</p>
-                </div>
-              ));
+            events.sort(
+              (a, b) =>
+                new Date(a.start).getTime() - new Date(b.start).getTime(),
+            );
+
+            return (
+              <div>
+                <h2>{name}</h2>
+                {events.map(
+                  ({id, name, feed, description, start, end, location}) => (
+                    <div key={id}>
+                      <h2>{name}</h2>
+                      <h3>Brought to you by {feed.name}</h3>
+                      <ul>
+                        <li>
+                          From {start} to {end}
+                        </li>
+                        <li>At {location}</li>
+                      </ul>
+                      <p>{description}</p>
+                    </div>
+                  ),
+                )}
+              </div>
+            );
           }}
         </Query>
       </div>
