d82b9d64c2df42d0706e526d46e58aca6fc0d2e1
Patrick Mifsud <patrickmifsud@outlook.com>
2019-07-28 02:17:05 +1000

    Tweaked Event Item

--- app/components/EventItem.tsx
+++ app/components/EventItem.tsx
@@ -29,9 +29,9 @@ export default class EventItem extends React.Component<EventItemProps, EventItem
             minute: 'numeric'
         };
         return <Container row style={[styles.Card, {borderColor: this.props.colour}]}>
-            <View style={{flex: 1}}>
+            <View style={{flex: 1, height: 75}}>
                 <Text style={styles.Heading}>{this.props.name}</Text>
-                <Text>{this.props.location}</Text>
+                <Text style={styles.SubHeading}>{this.props.location}</Text>
             </View>
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'flex-end', paddingRight: 10}}>
                 <Text style={styles.Dates}>{this.props.start.toLocaleString('en-GB', options)}</Text>
@@ -46,8 +46,8 @@ const styles = StyleSheet.create({
     Card: {
         padding: 10,
         borderLeftWidth: 5,
-        borderBottomColor: "#8a8a8a",
-        borderBottomWidth: 1
+        borderBottomColor: "#DFE1E5",
+        borderBottomWidth: .5
     },
 
     Heading: {
@@ -55,8 +55,14 @@ const styles = StyleSheet.create({
         color: "#000"
     },
 
+    SubHeading: {
+        fontSize: 16,
+        color: "#26282A"
+    },
+
     Dates: {
-        fontSize: 10
+        fontSize: 11,
+        color: "#3F3C42"
     },
 
     GeneralText: {},
