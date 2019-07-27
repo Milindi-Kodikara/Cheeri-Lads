3b7c5db459fdc46d2940cc0dc21ffa19692adcf6
Patrick Mifsud <patrickmifsud@outlook.com>
2019-07-27 21:54:26 +1000

    Created Basic Search View

--- app/screens/Search.tsx
+++ app/screens/Search.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CustomText from "../components/CustomText";
+import { SearchBar } from 'react-native-elements';
 
 
 interface SearchProps {
@@ -9,7 +10,26 @@ interface SearchState {
 }
 
 export default class Search extends React.Component<SearchProps, SearchState> {
+    state = {
+        search: '',
+    };
+
+    updateSearch = search => {
+        this.setState({ search });
+    };
+
+    // Need to embed this in to the nav bar
     render() {
-        return (<CustomText heading bold>S E A R C H</CustomText>)
+        const { search } = this.state;
+        return (
+            <SearchBar
+                platform="ios"
+                placeholder="Search..."
+                onChangeText={this.updateSearch}
+                value={search}
+            />
+        );
+        
+        // Need a List View Here, could re use the EventList view
     }
 }
